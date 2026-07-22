3eea227015cc94711597335e8fc85780c5b4ebc7
Milagros Gutierrez <m.gutierrez.dev5@gmail.com>
2026-07-22 20:07:49 -0300

    feat: declare book module interface

--- include/book.h
+++ include/book.h
@@ -7,4 +7,18 @@
 #define MAX_AUTHOR_LENGTH 100
 #define MAX_CATEGORY_LENGTH 50
 
+typedef struct {
+    int id;
+    char title[MAX_TITLE_LENGTH];
+    char author[MAX_AUTHOR_LENGTH];
+    int publicationYear;
+    char category[MAX_CATEGORY_LENGTH];
+    bool available;
+} Book;
+
+Book inputBook(void);
+bool isBookIdUnique(int id);
+void saveBook(Book book);
+void addBook(void);
+
 #endif
